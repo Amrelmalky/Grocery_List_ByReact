069bd7ec3151fcff95a3cbaf5fa637318e6d8e8f
Amrelmalky <69693889+Amrelmalky@users.noreply.github.com>
2022-12-25 22:27:44 +0200

    Updates

---
+++ precache-manifest.99bdfb67b7d3ed77f8d66cf6aa7cea0d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb650b85f2867256af072809412fa1ab",
+    "url": "/Grocery_List_ByReact/index.html"
+  },
+  {
+    "revision": "5bf07f1eb8cfffefef42",
+    "url": "/Grocery_List_ByReact/static/css/main.beecce7e.chunk.css"
+  },
+  {
+    "revision": "f86b35d4e5ed4a99a4db",
+    "url": "/Grocery_List_ByReact/static/js/2.22b6d6b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Grocery_List_ByReact/static/js/2.22b6d6b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bf07f1eb8cfffefef42",
+    "url": "/Grocery_List_ByReact/static/js/main.ecd49f98.chunk.js"
+  },
+  {
+    "revision": "85094b1cbb502edd8e49b981e744c7cf",
+    "url": "/Grocery_List_ByReact/static/js/main.ecd49f98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8360e667bebf1d736dc5",
+    "url": "/Grocery_List_ByReact/static/js/runtime-main.77ab580a.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Grocery_List_ByReact/precache-manifest.99bdfb67b7d3ed77f8d66cf6aa7cea0d.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Grocery_List_ByReact/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
